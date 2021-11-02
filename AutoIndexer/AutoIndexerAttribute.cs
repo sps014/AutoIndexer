9a558894e0233270a4ddc72712aed801ea675610
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-11-02 20:01:44 +0530

    Initial commit

---
+++ AutoIndexer/AutoIndexerAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IndexerGenerator
+{
+    public class AutoIndexerAttribute:Attribute
+    {
+        
+    }
+}
---
+++ IndexerGenerator/AutoIndexerGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace IndexerGenerator
+{
+    [Generator]
+    public class AutoIndexerGenerator:ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
