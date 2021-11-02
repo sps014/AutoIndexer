f81961b475eeebece6f4bea6a7157d65c6dff756
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-11-02 23:04:20 +0530

    code refactoring

--- IndexerGenerator/AutoIndexerGenerator.cs
+++ IndexerGenerator/AutoIndexerGenerator.cs
@@ -14,6 +14,9 @@ namespace IndexerGenerator
     [Generator]
     public class AutoIndexerGenerator:ISourceGenerator
     {
+        public const string ATTRIBUTE = "AutoIndexer";
+        public const string ATTRIBUTE_WITH_SYMBOL = "[AutoIndexer";
+
         public void Initialize(GeneratorInitializationContext context)
         {
         }
@@ -24,49 +27,66 @@ namespace IndexerGenerator
             var allTrees = context.Compilation.SyntaxTrees
                 .Where(t=>t.GetRoot().Language.Equals("C#"));
 
-            foreach (var t in allTrees)
-            {
-                var classes = t.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
+            var classes=GetAllClasses(allTrees);
+            var methods=GetAllMethods(classes);
 
-                foreach (var c in classes)
+            var @params=GetAllAttrbutedParamList(methods).ToList();
+            Console.WriteLine(@params[0]);
+        }
+
+        private IEnumerable<ClassDeclarationSyntax> GetAllClasses(IEnumerable<SyntaxTree> trees)
+        {
+            List<ClassDeclarationSyntax> classes = new List<ClassDeclarationSyntax>();
+            foreach(var t in trees)
+            {
+                if (!t.GetRoot().GetText().ToString().Contains(ATTRIBUTE_WITH_SYMBOL))
+                    continue;
+                classes.AddRange(t.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>());
+            }
+            return classes;
+        }
+        private IEnumerable<MethodDeclarationSyntax> GetAllMethods(IEnumerable<ClassDeclarationSyntax> @class)
+        {
+            var methods=new List<MethodDeclarationSyntax>();
+            foreach(var c in @class)
+                methods.AddRange(c.Members.OfType<MethodDeclarationSyntax>());
+            return methods;
+        }
+        private IEnumerable<IndexGeneratable> GetAllAttrbutedParamList(IEnumerable<MethodDeclarationSyntax> methods)
+        {
+            var generatables= new List<IndexGeneratable>();
+
+            foreach(var method in methods)
+            {
+
+                if (!method.GetText().ToString().Contains(ATTRIBUTE_WITH_SYMBOL))
+                    continue;
+
+                List<ParameterSyntax> parameters = new();
+
+                foreach(var parameter in method.ParameterList.Parameters)
                 {
-                    var methods=c.Members.OfType<MethodDeclarationSyntax>().ToList();
-                    foreach (var method in methods)
-                    {
-                        var prms= method.ParameterList.Parameters;
-                        List<ParameterSyntax> parameters= new List<ParameterSyntax>();
-                        foreach (var p in prms)
-                        {
-                            var v=p.AttributeLists.Select(a=>a.Attributes.Where(t=>t.Name.ToString().Contains("AutoIndexer"))).ToList();
-                            if(v.Count>=1)
-                            parameters.Add(p);
-                        }
-                        var Info = new ParamInfo
-                        {
-                            Class=c,
-                            Method=method,
-                            Parameters=parameters
-                        };
-                    }
+                    var hasAttrbute = parameter.AttributeLists
+                        .Any(a => a.Attributes
+                              .Any(at=>at.Name.ToString().Contains(ATTRIBUTE_WITH_SYMBOL))
+                              );
+                    if (hasAttrbute)
+                        parameters.Add(parameter);
                 }
+
+                generatables.Add(new IndexGeneratable
+                {
+                    Method = method,
+                    Parameters = parameters,
+                    Class=method.Parent as ClassDeclarationSyntax
+                });
+
             }
 
+            return generatables;
         }
 
-        private IEnumerable<ClassDeclarationSyntax> GetAllClasses(SyntaxTree tree)
-        {
-            return tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
-        }
-        private IEnumerable<MethodDeclarationSyntax> GetAllMethods(ClassDeclarationSyntax @class)
-        {
-            return @class.Members.OfType<MethodDeclarationSyntax>();
-        }
-        private SeparatedSyntaxList<ParameterSyntax> GetAllParams(MethodDeclarationSyntax method)
-        {
-            return method.ParameterList.Parameters;
-        }
-
-        public record ParamInfo
+        public record IndexGeneratable
         {
             public ClassDeclarationSyntax Class { get; set; } 
             public MethodDeclarationSyntax Method { get; set; }
