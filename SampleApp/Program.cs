35d7e2225200988df8047eb425e0ce511860c85a
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-11-04 19:08:15 +0530

    Update Program.cs

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -10,11 +10,13 @@ namespace Try
     public class DataStore<T>
     {
         public int Count { get; set; }
-        public void AddAt([AutoIndexer] int idx, [AutoIndexer] int y)
+        public void AddAt([AutoIndexer] int idx, 
+            [AutoIndexer] int y)
         {
             Console.WriteLine(idx);
         }
-        public void RemoveAt([AutoIndexer] int idx, [AutoIndexer] double z,string k,float f)
+        public void RemoveAt([AutoIndexer] int idx,
+            [AutoIndexer] double z,string k,float f)
         {
             Console.WriteLine(idx);
         }
